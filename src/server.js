5a5f5b46ab09dd6a56bd3770041aa00ced136021
Victoria-cyb <chiamakamichael58@gmail.com>
2025-08-14 08:50:56 +0100

    initial commit

--- src/server.js
+++ src/server.js
@@ -32,7 +32,7 @@ const authMiddleware = (req, res, next) => {
   next();
 };
 
-app.use(cors());
+app.use(cors({ origin: 'http://127.0.0.1:5500' }));
 app.use(express.json());
 app.use(authMiddleware);
 
